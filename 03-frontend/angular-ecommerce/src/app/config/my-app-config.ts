bcdf49eb8b7cf72c8825d7072d2f4e32a7d7926f
KuRoUsaG1 <allen.liuyu@gmail.com>
2023-08-27 20:25:13 -0700

    starting on login pages

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: 'JxFoCpGK8vQPQmVoF60vb74FxTK7ht5h',
+        issuer: 'https://dev-dbbkgbawrlv3kenf.us.auth0.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    },
+}
