2ae64a7cf53220548f6b104abff53792d5e14678
KuRoUsaG1 <allen.liuyu@gmail.com>
2023-08-27 22:51:26 -0700

    finishing okta work

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc: {
-        clientId: 'JxFoCpGK8vQPQmVoF60vb74FxTK7ht5h',
-        issuer: 'https://dev-dbbkgbawrlv3kenf.us.auth0.com/oauth2/default',
+        clientId: '0oaazz5ba0PTj8v7q5d7',
+        issuer: 'https://dev-97952251.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     },
